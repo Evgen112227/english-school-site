7bdcb257c23d5b1c3fdcd3e4ccda6bb63031d5cd
Yauheni Hladki <Deribasovich@gmail.com>
2022-11-11 17:51:51 +0300

    added slider section

---
+++ js/script.js
@@ -0,0 +1,50 @@
+new Swiper('.image-slider', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	slidesPerView: 3,
+	keyboard: {
+		enabled: true,
+		onlyInViewport: false,
+	},
+	loop: true,
+	autoplay: {
+		delay: 2000,
+		disabledOnInteraction: true,
+	},
+	speed: 800,
+	// direction: 'vertical',
+	// effect: 'cube',
+	// cubeEffect: {
+	// 	slideShadows: true,
+	// 	shadow: true,
+	// 	shadowOffset: 20,
+	// 	shadowScale: 0.94,
+	// },
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		600: {
+			slidesPerView: 2,
+		},
+		992: {
+			slidesPerView: 3,
+		},
+	},
+	preloadImages: false,
+	// Enable lazy loading
+	lazy: {
+		loadOnTransitionStart: false,
+		loadPrevNext: true,
+	},
+	watchSlidesProgress: true,
+	a11y: {
+		enabled: true,
+		prevSlideMessage: 'Previous slide',
+		nextSlideMessage: 'Next slide',
+		firstSlideMessage: 'This is the first slide',
+		lastSlideMessage: 'This is the last slide',
+	},
+});
