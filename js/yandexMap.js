f095bcb7dc74b49580b253b64f89cba4ae9ae9dd
Yauheni Hladki <Deribasovich@gmail.com>
2022-11-30 14:01:40 +0300

    added some changes

--- js/yandexMap.js
+++ js/yandexMap.js
@@ -2,7 +2,7 @@ let center = [53.90617438400433, 27.542316398147573];
 
 function init() {
 	let map = new ymaps.Map('map', {
-		center: center,
+		center,
 		zoom: 17,
 	});
 
@@ -11,12 +11,16 @@ function init() {
 	map.controls.remove('geolocationControl'); // удаляем геолокацию
 	map.controls.remove('searchControl'); // удаляем поиск
 	map.controls.remove('trafficControl'); // удаляем контроль трафика
-	map.controls.remove('typeSelector'); // удаляем тип
+	// map.controls.remove('typeSelector'); удаляем тип
 	map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
 	// map.controls.remove('zoomControl'); // удаляем контрол зуммирования
 	map.controls.remove('rulerControl'); // удаляем контрол правил
 
 	map.geoObjects.add(placemark);
+
+	map.balloon.open([53.90635, 27.5423], {
+		contentHeader: 'New Challenge',
+	});
 }
 
 ymaps.ready(init);
